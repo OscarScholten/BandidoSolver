4c6d96ae3b662bcf9b9350b67563f27d177ecd08
Oscar Scholten <oscar.scholten@hr2day.com>
2024-01-24 16:06:49 +0100

    Some initial tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,46 @@
+/* tile elements (from https://www.w3.org/TR/xml-entity-names/025.html)
+ * 2501 ━ BOX DRAWINGS HEAVY HORIZONTAL
+ * 2578 ╸ BOX DRAWINGS HEAVY LEFT
+ * 257A ╺ BOX DRAWINGS HEAVY RIGHT
+ * 250F ┏ BOX DRAWINGS HEAVY DOWN AND RIGHT
+ * 2513 ┓ BOX DRAWINGS HEAVY DOWN AND LEFT
+ * 2533 ┳ BOX DRAWINGS HEAVY DOWN AND HORIZONTAL
+ * 2517 ┗ BOX DRAWINGS HEAVY UP AND RIGHT
+ * 251B ┛ BOX DRAWINGS HEAVY UP AND LEFT
+ * 253B ┻ BOX DRAWINGS HEAVY UP AND HORIZONTAL
+ * 2523 ┣ BOX DRAWINGS HEAVY VERTICAL AND RIGHT
+ * 252B ┫ BOX DRAWINGS HEAVY VERTICAL AND LEFT
+ * 254B ╋ BOX DRAWINGS HEAVY VERTICAL AND HORIZONTAL
+ */
+
+class TileData {
+    readonly up: boolean;
+    readonly right: boolean;
+    readonly down: boolean;
+    readonly left: boolean;
+    constructor(up: boolean, right: boolean, down: boolean, left: boolean) {
+        this.up = up;
+        this.right = right;
+        this.down = down;
+        this.left = left;
+    }
+}
+
+const tileData = new Map<string, TileData>([
+    ['━', new TileData(false, true, false, true)],
+    ['╸', new TileData(false, false, false, true)],
+    ['╺', new TileData(false, true, false, false)],
+    ['┏', new TileData(false, true, true, false)],
+    ['┓', new TileData(false, false, true, true)],
+    ['┳', new TileData(false, true, true, true)],
+    ['┗', new TileData(true, true, false, false)],
+    ['┛', new TileData(true, false, false, true)],
+    ['┻', new TileData(true, true, false, true)],
+    ['┣', new TileData(true, true, true, false)],
+    ['┫', new TileData(true, false, true, true)],
+    ['╋', new TileData(true, true, true, true)]
+]);
+
 export class Game {
     readonly board: string;
     readonly deck: string;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,25 @@
 import { Game, SolveResult, solve } from '../src/index';
 
 describe('testing solving', () => {
-    test('solving an empty deck is not possible', () => {
+    test('solving an empty board is a success', () => {
         const game = new Game('', '');
         const solveResult = solve(game);
+        expect(solveResult.success).toBe(true);
+    });
+    test('solving a board with no open tiles is a success', () => {
+        const game = new Game('╺╸', '');
+        const solveResult = solve(game);
+        expect(solveResult.success).toBe(true);
+    });
+    test('solving a board with open tiles and no deck cannot be done', () => {
+        const game = new Game('━━', '');
+        const solveResult = solve(game);
         expect(solveResult.success).toBe(false);
+    });
+    test('solving a board with open tiles and a basic deck is a success', () => {
+        const game = new Game('━━', '━━ ━━ ━━ ━━ ━╸ ━╸');
+        const solveResult = solve(game);
+        expect(solveResult.success).toBe(true);
+        expect(solveResult.board).toBe('╺━━━━╸');
     });
 });
